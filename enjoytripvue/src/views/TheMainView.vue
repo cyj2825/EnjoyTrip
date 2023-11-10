7e5b068a4a43e05dd0ff8262dc94df5a2ee6f59d
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-11-10 17:57:18 +0900

    feat: main 수정중
    Former-commit-id: 0bc236bab5ea61026f593d09589463b99430423b

--- enjoytripvue/src/views/TheMainView.vue
+++ enjoytripvue/src/views/TheMainView.vue
@@ -1,7 +1,3 @@
-<script setup></script>
-
-<template>
-  <div>Main 화면 입니다.</div>
-</template>
-
-<style scoped></style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:35f7217bea2bb063497a7ba10b45585c5463efdc9964989751fb5a5170ff1590
+size 906
