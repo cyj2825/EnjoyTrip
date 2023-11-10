e8c386b892cfc7291730934b606f79dc37530187
조예진 <cyj2825@naver.com>
2023-11-10 17:54:50 +0900

    feat : board 수정 중
    Former-commit-id: d96da80098b1b59902d7653ccad5eaeb6d7b072b

--- enjoytripspring/src/main/java/com/ssafy/enjoytrip/board/service/BoardService.java
+++ enjoytripspring/src/main/java/com/ssafy/enjoytrip/board/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57c164e8cea51a2333b654e139225b38d9515820a055f5f937adc3c5502221fb
-size 530
+oid sha256:4a95ce9d389accc1a0cd60f1cec3437352f779cdd8effa9dcc7940573aaacaf4
+size 528
