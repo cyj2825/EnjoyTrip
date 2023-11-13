fbd2119be428ba015460016a5a2e4e526eb6c38d
조예진 <cyj2825@naver.com>
2023-11-13 17:43:04 +0900

    feat : sql 파일 추가 및 plan CRUD 시작
    Former-commit-id: fc1aea9bcfaeaf3e6b1ba8060774931aab72e3d6

---
+++ enjoytripspring/src/main/java/com/ssafy/enjoytrip/plan/model/dto/PlanDateDetailDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e21b5508f465131767a98c07ccbd21281f419ac8d540477fc2712029784efe9
+size 627
---
+++ enjoytripspring/src/main/java/com/ssafy/enjoytrip/plan/model/service/PlanService.java
@@ -0,0 +1,16 @@
+package com.ssafy.enjoytrip.plan.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ssafy.enjoytrip.plan.model.dto.PlanDto;
+import com.ssafy.enjoytrip.util.PageNavigation;
+
+public interface PlanService {
+	void writePlan(PlanDto planDto);
+	List<PlanDto> listPlan(Map<String, Object> param);
+	PlanDto getPlan(int plan_no);
+	void modifyPlan(PlanDto planDto);
+	void deletePlan(int plan_no);
+	PageNavigation makePageNavigation(Map<String, String> map);
+}
