82c34b52749c0f6712bd12d720c4601252e3dba6
조예진 <cyj2825@naver.com>
2023-11-13 16:30:07 +0900

    feat : 게시글 댓글 CRUD 완료
    Former-commit-id: 261d836a730d5529d3fde66294db706592a97b29

--- enjoytripspring/src/main/java/com/ssafy/enjoytrip/board/service/BoardCommentService.java
+++ enjoytripspring/src/main/java/com/ssafy/enjoytrip/board/service/BoardCommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47a15a5472a6b5a19cf65b94013beee990eea73d9a3cc48079cad0010c1e60ee
-size 506
+oid sha256:c3234631932d92946ba089ea81215e67552918f7b072be17d4cbcc9d5ade0131
+size 511
