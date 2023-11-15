28ae23b36e418b1a432523888c4ed2973cb068a7
조예진 <cyj2825@naver.com>
2023-11-15 17:43:37 +0900

    update : 로그인 화면 수정 및 파일 이동
    Former-commit-id: 9510c15147a22556b0d7f34dc013cf1b27683d72

--- enjoytripvue/src/components/board/item/LoginModal.vue
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:23246dbc7981efc0cc4c620f1463882089d2f1895c451bad1c3ae529b7e863b5
-size 3250
---
+++ enjoytripvue/src/components/modal/LoginModal.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:da124380bc1e57f5ade976a6d7923a0d016b5808db503475cf6d2751492f6358
+size 3234
