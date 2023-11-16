43b788f236f19857105135af5450d38f1933e482
조예진 <cyj2825@naver.com>
2023-11-16 17:49:01 +0900

    update : BoardDto 및 DB 수정 완료
    Former-commit-id: b4a06f8deb1be63ac6755aa3ea61427cfcb49fb2

--- enjoytripspring/src/main/resources/db/ssafyenjoydb.sql
+++ enjoytripspring/src/main/resources/db/ssafyenjoydb.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc7f93687ccc349be1966c9376b168c3e87b18a6b7ad33133ad81e77f7de17e3
-size 6519
+oid sha256:adce45fa05034225fe0f15009beb41e19137aed4ee45bb4f518b6f14d9342945
+size 6838
