410709c2fccd811ffdfdcde4cbf8baf27ad1d54a
조예진 <cyj2825@naver.com>
2023-11-17 17:59:23 +0900

    feat : 여행 후기 작성 서버 연동 수정
    Former-commit-id: a52a1c451addb5b0e89c239796087540ff0e8403

--- enjoytripvue/src/api/user.js
+++ enjoytripvue/src/api/user.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcb246cc7943fd8454cbabb1f21745988899a5eeee9a84d4e37e54a4ff0aed47
-size 1403
+oid sha256:2d2bb82f38f9e5abb9d93412ec1f90c7d3334788c9af2e9c2eb668bcbcc43dd5
+size 1401
--- enjoytripvue/src/components/board/BoardDetail.vue
+++ enjoytripvue/src/components/board/BoardDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f58aecaeb28f50312c73d2510fd24412840569a15f06854aff013f35f7871853
-size 8518
+oid sha256:e4ecb8b460d10b8785085d2b6763b23fa03d39d273ac414e0cd61c5f02ed4fb5
+size 8702
