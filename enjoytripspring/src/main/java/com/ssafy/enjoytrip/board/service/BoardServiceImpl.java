4e3fb1023b5028d343f366f0b6144e1dffa1347a
조예진 <cyj2825@naver.com>
2023-11-17 16:13:50 +0900

    feat : 여행 후기 작성 수정 완료
    Former-commit-id: 8cd26aa831ea1d1a2a5cbb9c6c8e22266d33cda5

--- enjoytripspring/src/main/java/com/ssafy/enjoytrip/board/service/BoardServiceImpl.java
+++ enjoytripspring/src/main/java/com/ssafy/enjoytrip/board/service/BoardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2991555a9e5f6f3a456d82c8a3ab0b7a7a0aff39a91c600630eda3b50ee08ac1
-size 3004
+oid sha256:0b23a625c03f1309313ca494fc6ca411dda2fd8c038a417369d3dffa7e2d6e96
+size 3136
