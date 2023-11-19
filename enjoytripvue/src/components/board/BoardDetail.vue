4581ad4782ff9b0d57886da2218bb5ba46edd7d2
조예진 <cyj2825@naver.com>
2023-11-19 17:22:01 +0900

    feat : 여행 후기 리스트 화면 수정 중
    Former-commit-id: 8ecd9b51ab0df1fd6b52e02df9f72983ce736891

--- enjoytripvue/src/components/board/BoardDetail.vue
+++ enjoytripvue/src/components/board/BoardDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4ecb8b460d10b8785085d2b6763b23fa03d39d273ac414e0cd61c5f02ed4fb5
-size 8702
+oid sha256:20b5250f50061210ec97f891fb219c22330ddc3918f42eaaf148f19ae3ea8da1
+size 8694
--- enjoytripvue/src/components/common/VSelect.vue
+++ enjoytripvue/src/components/common/VSelect.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:271a439b690de7fd51700d5ada6943adb3305947462a56b87cde7d2116905b3a
-size 649
+oid sha256:b428ccf76167ce644ced3114fbea797a9f196315a74298a4794fd957b1f5fde0
+size 660
