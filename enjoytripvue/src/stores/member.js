bc5616081d8ccfc5ae8808626641b6574d06594d
조예진 <cyj2825@naver.com>
2023-11-20 18:01:57 +0900

    feat : 마이페이지 수정 중
    Former-commit-id: 5ad03868d6b6415664b12d0bb97fb5a3f59e8cde

--- enjoytripvue/src/stores/member.js
+++ enjoytripvue/src/stores/member.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ec6e3fef972a24ce978be8473eb8525922f460d5f1c73db0e49d8492bf71c0e
-size 4718
+oid sha256:2a49d621f811c1fe4570b9dc2e7a12d44d0391323e79dda195fe4372ebc137ed
+size 4555
