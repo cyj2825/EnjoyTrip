d8e816b71b0bb312c89a26e4600383e63c996f98
조예진 <cyj2825@naver.com>
2023-11-20 16:58:25 +0900

    feat : 내비바 수정 중
    Former-commit-id: b8877835b75194b8c036773a3b7e7faead5810de

--- enjoytripvue/src/components/layout/TheHeadingNavbar.vue
+++ enjoytripvue/src/components/layout/TheHeadingNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc5c8abd0cf863e1f623d7baa90d906f73c8e2b2cebb61d28615de17f72d7ee7
-size 4368
+oid sha256:6936220de5e09f9239542a894fad9ef847e44e591cd4a104a0cb2f8d04d78db7
+size 4909
