7158dfead2ecd1289fa37404bc312938171d3dc2
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-11-21 09:01:16 +0900

    feat : 메뉴바 긴급수정
    Former-commit-id: cb94aac7869b191f2e9a198d07446be227f3aed7

--- enjoytripvue/src/components/layout/TheHeadingNavbar.vue
+++ enjoytripvue/src/components/layout/TheHeadingNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1203854b1031d98a9546015541507814f942b9e7196c31aa9750151c3d59ebc
-size 7048
+oid sha256:c80be7a987c2e7dd4b4d7af864d2436707e6e78d266a1634fd6c7032c9aa5fad
+size 7024
