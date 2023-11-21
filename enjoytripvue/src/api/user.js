0533f36dad689b088598681f21da0c4262e832e2
조예진 <cyj2825@naver.com>
2023-11-21 22:58:56 +0900

    update : 마이페이지 프로필 이미지 띄우기 완료
    Former-commit-id: 0d799331f9442922522bab9707443bf85193a5b4

--- enjoytripvue/src/api/user.js
+++ enjoytripvue/src/api/user.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b52cf0c9f2ac11bd61ca94470f2099660093ae25b2ab12e8b5bb46d90ac7ade
-size 2048
+oid sha256:7b5aec2082fba9c6332f87b0a0ff4d84eeca16bc64cd5802240cdbe7a45014c2
+size 2217
