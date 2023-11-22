bfcaec0ae3d9450530b1e5d80762501f4ce7bfac
조예진 <cyj2825@naver.com>
2023-11-22 20:46:20 +0900

    feat : 계획페이지 구현 중
    Former-commit-id: 769eb0e6d549bef8f1937a080d56ffac83fd7645

--- enjoytripvue/src/views/TheMainView.vue
+++ enjoytripvue/src/views/TheMainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:659e1b1559b3183b1490e2f1d4dcd9ca1fb5776e747d5ab0a33836e0a2c7e403
-size 3134
+oid sha256:e322af57ed850042b4c9ff89940efdc5ca107a458617c42ff28472ab93fca192
+size 3185
