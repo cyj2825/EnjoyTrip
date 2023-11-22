8818c5abe93ac89d771cbee3949519a164474505
조예진 <cyj2825@naver.com>
2023-11-22 17:44:32 +0900

    feat : 프로필 이미지 수정 추가
    Former-commit-id: 2b7988a185ef858c5f588f60fa9c898df56747f0

--- enjoytripspring/src/main/java/com/ssafy/enjoytrip/member/service/ProfileService.java
+++ enjoytripspring/src/main/java/com/ssafy/enjoytrip/member/service/ProfileService.java
@@ -1,9 +1,3 @@
-package com.ssafy.enjoytrip.member.service;
-
-import com.ssafy.enjoytrip.member.model.dto.ProfileImgDto;
-
-public interface ProfileService {
-	void insertImg(ProfileImgDto profileImgDto);
-	String getImg(String userId);
-//	void deleteMember(String userId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:22335ce77f582d330f04de9077b93a8909507b2d286128e391e3d01a6993adb2
+size 300
