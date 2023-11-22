6cdf5e90789e39fd34925ea0956e1bab342d0e31
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-11-22 15:59:41 +0900

    feat : 메뉴바 수정
    Former-commit-id: 81bb3068b8c33f7751af0af7fefc837ddd56895e

--- enjoytripvue/src/components/layout/TheHeadingNavbar.vue
+++ enjoytripvue/src/components/layout/TheHeadingNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd94a32d67a481fa15c5b9bcf85ff345e347660a850c645463711ebb5c881764
-size 3438
+oid sha256:e6440b87d5a49cd22321a312abf49068e7b3d3ded57d23baf9796096461df944
+size 3525
---
+++ enjoytripvue/src/components/plan/PlanList.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="plan-list-container">
+
+  </div>
+</template>
+
+<style>
+#plan-list-container{
+  height: 100vh;
+  width: 100vw;
+  
+}
+</style>
