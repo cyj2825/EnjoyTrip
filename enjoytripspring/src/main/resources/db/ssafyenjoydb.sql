653530ae5d99bcb24c34dd1b65492abcf2ee2d3c
조예진 <cyj2825@naver.com>
2023-11-23 02:11:20 +0900

    add : DB 데이터 추가
    Former-commit-id: 77879f4716677f247f083c2f0aa28ddabc8e2a54

--- enjoytripspring/src/main/resources/db/ssafyenjoydb.sql
+++ enjoytripspring/src/main/resources/db/ssafyenjoydb.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c00abbc6fa1c3ac06777b51daece37089d0fc65504b0e1abe59593f62b09c785
-size 7509
+oid sha256:0f5aefb5582971bb902cf814b7eecbed51d659cce6cdb3a6ed4048f524d2e8ab
+size 7508
