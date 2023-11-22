fe46aec6e1e8d29dd98ee0b667610b2d09e5c20c
조예진 <cyj2825@naver.com>
2023-11-22 21:50:13 +0900

    feat : 챗봇 기능 추가
    Former-commit-id: eed5ae59ae2488e36d3d8862b80525e96e621fee

--- enjoytripvue/src/App.vue
+++ enjoytripvue/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc382bf1eb7349c6f45b8480b7c4a0ed902577678735d4e31a8f2343562b02ed
-size 1577
+oid sha256:55651c9c5b8a6209bfdd42125091d37c773b8d4bde0bd1e79170f41e710eff6c
+size 2507
