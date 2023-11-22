f5d3aa19ea6a57ca346be766b150bf02cc89be48
조예진 <cyj2825@naver.com>
2023-11-22 17:56:35 +0900

    add : plan 코드 추가
    Former-commit-id: 9a7f02051347b56c1d6f54dd7b7efc9c38195fe2

--- enjoytripvue/src/components/plan/PlanList.vue
+++ enjoytripvue/src/components/plan/PlanList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31e54e2fc106d42e9142d6faaf10f4ccd1bc1a2594493a6fc32ea687af681a9f
-size 650
+oid sha256:4e51dbe28b11683ce14784a9369621449f81d1b4361d65767e3a9175a31d2662
+size 2030
