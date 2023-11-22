095901a166b21915e0217cd9cdb9e73f816e8df0
조예진 <cyj2825@naver.com>
2023-11-22 20:46:20 +0900

    feat : 계획페이지 구현 중
    Former-commit-id: 8f2fb3af5afb9009ab2d3d1a371501ab800b59be

--- enjoytripvue/src/components/plan/item/PlanFormItem.vue
+++ enjoytripvue/src/components/plan/item/PlanFormItem.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3021db07cb6d2b6cdd4cb7e61489fc8fe70d4d921997b37b148156003a2cb849
-size 881
+oid sha256:17ceec2d7da35f6bd194a4c716cad17183aad3b54905e60877a2719e632b7bba
+size 1634
--- enjoytripvue/src/router/index.js
+++ enjoytripvue/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e966b42cddc81a0a0d62cbb76ee248778dd4aa094caccb88f477a4ada0870fdc
-size 3020
+oid sha256:2eaba24d61e050a11035b3925e02d3c0763c0e0f30aae21403055cf663362b76
+size 3180
