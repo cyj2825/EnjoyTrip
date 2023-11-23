78681e9616f2f6562cb535129a6d6dc84af3882c
조예진 <cyj2825@naver.com>
2023-11-23 15:27:37 +0900

    feat : 메인페이지 구현
    Former-commit-id: 9e60e09a4ee4a11ae7674249841b7c573dfea58a

--- enjoytripvue/src/components/plan/PlanDetail.vue
+++ enjoytripvue/src/components/plan/PlanDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24a5ea1c756af5a03155f5cb5c3412f5fe63a9260b23a497f0e20b45987efe79
-size 4328
+oid sha256:a4af64fd43c05e0be3da07ccc33ca52c5a49fed6b04f7ceb8c10d9aad75b6591
+size 4319
--- enjoytripvue/src/components/plan/item/PlanDateItem.vue
+++ enjoytripvue/src/components/plan/item/PlanDateItem.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c27aec04166687e48dba1497e68894b331da75d7fe80aef9d6973e65535c5c3e
-size 755
+oid sha256:4f522eb4b18aaeed936d6b1cc8fcb6bb370dbbb294352922898ed63fab25144b
+size 751
